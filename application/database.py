61fa5e4a90056f1127130e79c294ffd2abb2701b
ramintheredmn <redcrossprog@gmail.com>
2023-08-17 17:29:58 +0330

    modify

--- application/database.py
+++ application/database.py
@@ -2,10 +2,11 @@ from sqlalchemy import create_engine
 
 
 
-engine = create_engine(
-
-)
-
-
-
-
+engine = create_engine("mysql+pymysql://xvbut0yxbmx0kyrtkkta:pscale_pw_ls2MOUHaVLNOutqlfRLWM3aJQQFNf5xFjyagS4CdHIg@aws.connect.psdb.cloud/gadget-test?charset=utf8mb4",
+                       connect_args={
+                           
+                           "ssl": {
+                                "ssl_ca": "/etc/ssl/cert.pem",
+                                
+                           
+                       }})
