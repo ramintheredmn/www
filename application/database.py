edc6d9cf9f583c694909e24599fad983e554f5b7
Ramin Rafiee <73221830+ramintheredmn@users.noreply.github.com>
2023-08-17 17:31:41 +0330

    Update database.py

--- application/database.py
+++ application/database.py
@@ -2,11 +2,5 @@ from sqlalchemy import create_engine
 
 
 
-engine = create_engine("mysql+pymysql://xvbut0yxbmx0kyrtkkta:pscale_pw_ls2MOUHaVLNOutqlfRLWM3aJQQFNf5xFjyagS4CdHIg@aws.connect.psdb.cloud/gadget-test?charset=utf8mb4",
-                       connect_args={
-                           
-                           "ssl": {
-                                "ssl_ca": "/etc/ssl/cert.pem",
-                                
-                           
-                       }})
+engine = create_engine(
+)
