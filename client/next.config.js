5328503240c6e2a360496d9c1d9792ef422c73a3
ramintheredmn <redcrossprog@gmail.com>
2023-08-28 22:41:54 +0430

    .

--- client/next.config.js
+++ client/next.config.js
@@ -17,5 +17,5 @@ const nextConfig = {
 module.exports = nextConfig
 
 // when you wanna build for production use the 3rd line 
-// be sure that is not commented for development with flask dev server opned in 5000 port 
+// be sure that is not commented. for development with flask dev server opned in 5000 port 
 // be sure that the 3 line is commented and 4 to 14 is not commented
