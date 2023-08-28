6fd1c42f21c0b4ae93ee875349b25ffd4f9fbfba
ramintheredmn <redcrossprog@gmail.com>
2023-08-28 21:40:19 +0330

    swithced to apexcharts

--- client/pages/index.js
+++ client/pages/index.js
@@ -11,8 +11,11 @@ import Navabr from './components/navbar';
 export default function Home() {
   return(
     <>
-    <div className='w-full h-full'>
+    
+    <div className='bg-base-100 w-full h-full'>
     <Navabr title="Homepage" />
+  
+    <div className='flex content-center items-center justify-center bg-base-100 text-xl'><p className='bg-base-100 mt-5 justify-center'>From the navbar menu select Chart demo</p></div>
     </div>
     </>
   )
